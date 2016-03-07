6b99e64dd9cb19ad0459b4ade2884faaa9b4fbff
quynv <quytnhanam@gmail.com>
2016-03-07 08:27:53 -0800

    Connect to TMDB

--- frontend/views/layouts/slider.php
+++ frontend/views/layouts/slider.php
@@ -6,150 +6,47 @@ use yii\helpers\Url;
 <div class="ms-layers-template">
     <!-- masterslider -->
     <div class="master-slider ms-skin-black-2 round-skin" id="masterslider">
-        <div class="ms-slide">
-            <?= Html::img('@web/js/plugins/masterslider/style/blank.gif',['data-src'=>Url::to('@web/img/image1.jpg')])?>
+        <?php foreach( $movies as $key => $movie ){?>
+            <div class="ms-slide">
+                <?= Html::img('@web/js/plugins/masterslider/style/blank.gif',
+                    ['data-src'=>Url::to($movie->getBackdrop('w1280'))])
+                ?>
 
-<!--            <video data-autopause="false" data-mute="true" data-loop="true" data-fill-mode="fill">-->
-<!--                <source id="mp4" src="http://media.w3.org/2010/05/bunny/trailer.mp4" type="video/mp4"/>-->
-<!--                <source id="ogv" src="http://media.w3.org/2010/05/bunny/trailer.ogv" type="video/ogg"/>-->
-<!--            </video>-->
+                <!--            <video data-autopause="false" data-mute="true" data-loop="true" data-fill-mode="fill">-->
+                <!--                <source id="mp4" src="http://media.w3.org/2010/05/bunny/trailer.mp4" type="video/mp4"/>-->
+                <!--                <source id="ogv" src="http://media.w3.org/2010/05/bunny/trailer.ogv" type="video/ogg"/>-->
+                <!--            </video>-->
 
-            <h3 class="ms-layer ms-promo-info color-light"  style="left:10px; top:170px"
-                data-effect="bottom(20)"
-                data-duration="2300"
-                data-delay="2300"
-                data-ease="easeOutExpo"
-                >AMAZING</h3>
+                <h3 class="ms-layer ms-promo-info color-light"  style="left:10px; top:170px; color: #fff!important;"
+                    data-effect="bottom(20)"
+                    data-duration="2300"
+                    data-delay="2300"
+                    data-ease="easeOutExpo"
+                    >Now playing</h3>
 
-            <h3 class="ms-layer ms-promo-info-in color-light"  style="left:10px; top:245px"
-                data-effect="left(100)"
-                data-duration="3300"
-                data-delay="1900"
-                data-ease="easeOutExpo"
-                ><span class="color-green">FEATURES</span></h3>
+                <h3 class="ms-layer ms-promo-info-in color-light"  style="left:10px; top:245px"
+                    data-effect="left(100)"
+                    data-duration="3300"
+                    data-delay="1900"
+                    data-ease="easeOutExpo"
+                    ><span class="color-green"><?= $movie->getTitle() ?></span></h3>
 
-            <h3 class="ms-layer normal-title color-light"  style="left:10px; top:312px"
-                data-effect="bottom(20)"
-                data-duration="2300"
-                data-delay="2000"
-                data-ease="easeOutExpo"
-                >UNIFY BOOTSTRAP TEMPLATE</h3>
+                <h3 class="ms-layer normal-title color-light"  style="left:10px; top:312px; color: #fff!important;"
+                    data-effect="bottom(20)"
+                    data-duration="2300"
+                    data-delay="2000"
+                    data-ease="easeOutExpo"
+                    >Release at <?= $movie->getReleaseDate()?></h3>
 
-            <p class="ms-layer ms-promo-sub ms-promo-sub-in color-light"  style="left:10px; top:360px"
-               data-effect="right(40)"
-               data-duration="2300"
-               data-delay="2300"
-               data-ease="easeOutExpo"
-                >YOUTUBE, VIMEO AND CUSTOM <br> IFRAME SUPPORTED</p>
-        </div>
-        <div class="ms-slide">
-            <?= Html::img('@web/js/plugins/masterslider/style/blank.gif',['data-src'=>Url::to('@web/img/image2.jpg')])?>
-
-            <!--            <video data-autopause="false" data-mute="true" data-loop="true" data-fill-mode="fill">-->
-            <!--                <source id="mp4" src="http://media.w3.org/2010/05/bunny/trailer.mp4" type="video/mp4"/>-->
-            <!--                <source id="ogv" src="http://media.w3.org/2010/05/bunny/trailer.ogv" type="video/ogg"/>-->
-            <!--            </video>-->
-
-            <h3 class="ms-layer ms-promo-info color-light"  style="left:10px; top:170px"
-                data-effect="bottom(20)"
-                data-duration="2300"
-                data-delay="2300"
-                data-ease="easeOutExpo"
-                >AMAZING</h3>
-
-            <h3 class="ms-layer ms-promo-info-in color-light"  style="left:10px; top:245px"
-                data-effect="left(100)"
-                data-duration="3300"
-                data-delay="1900"
-                data-ease="easeOutExpo"
-                ><span class="color-green">FEATURES</span></h3>
-
-            <h3 class="ms-layer normal-title color-light"  style="left:10px; top:312px"
-                data-effect="bottom(20)"
-                data-duration="2300"
-                data-delay="2000"
-                data-ease="easeOutExpo"
-                >UNIFY BOOTSTRAP TEMPLATE</h3>
-
-            <p class="ms-layer ms-promo-sub ms-promo-sub-in color-light"  style="left:10px; top:360px"
-               data-effect="right(40)"
-               data-duration="2300"
-               data-delay="2300"
-               data-ease="easeOutExpo"
-                >YOUTUBE, VIMEO AND CUSTOM <br> IFRAME SUPPORTED</p>
-        </div>
-        <div class="ms-slide">
-            <?= Html::img('@web/js/plugins/masterslider/style/blank.gif',['data-src'=>Url::to('@web/img/image3.jpg')])?>
-
-            <!--            <video data-autopause="false" data-mute="true" data-loop="true" data-fill-mode="fill">-->
-            <!--                <source id="mp4" src="http://media.w3.org/2010/05/bunny/trailer.mp4" type="video/mp4"/>-->
-            <!--                <source id="ogv" src="http://media.w3.org/2010/05/bunny/trailer.ogv" type="video/ogg"/>-->
-            <!--            </video>-->
-
-            <h3 class="ms-layer ms-promo-info color-light"  style="left:10px; top:170px"
-                data-effect="bottom(20)"
-                data-duration="2300"
-                data-delay="2300"
-                data-ease="easeOutExpo"
-                >AMAZING</h3>
-
-            <h3 class="ms-layer ms-promo-info-in color-light"  style="left:10px; top:245px"
-                data-effect="left(100)"
-                data-duration="3300"
-                data-delay="1900"
-                data-ease="easeOutExpo"
-                ><span class="color-green">FEATURES</span></h3>
-
-            <h3 class="ms-layer normal-title color-light"  style="left:10px; top:312px"
-                data-effect="bottom(20)"
-                data-duration="2300"
-                data-delay="2000"
-                data-ease="easeOutExpo"
-                >UNIFY BOOTSTRAP TEMPLATE</h3>
-
-            <p class="ms-layer ms-promo-sub ms-promo-sub-in color-light"  style="left:10px; top:360px"
-               data-effect="right(40)"
-               data-duration="2300"
-               data-delay="2300"
-               data-ease="easeOutExpo"
-                >YOUTUBE, VIMEO AND CUSTOM <br> IFRAME SUPPORTED</p>
-        </div>
-        <div class="ms-slide">
-            <?= Html::img('@web/js/plugins/masterslider/style/blank.gif',['data-src'=>Url::to('@web/img/image4.jpg')])?>
-
-            <!--            <video data-autopause="false" data-mute="true" data-loop="true" data-fill-mode="fill">-->
-            <!--                <source id="mp4" src="http://media.w3.org/2010/05/bunny/trailer.mp4" type="video/mp4"/>-->
-            <!--                <source id="ogv" src="http://media.w3.org/2010/05/bunny/trailer.ogv" type="video/ogg"/>-->
-            <!--            </video>-->
-
-            <h3 class="ms-layer ms-promo-info color-light"  style="left:10px; top:170px"
-                data-effect="bottom(20)"
-                data-duration="2300"
-                data-delay="2300"
-                data-ease="easeOutExpo"
-                >AMAZING</h3>
-
-            <h3 class="ms-layer ms-promo-info-in color-light"  style="left:10px; top:245px"
-                data-effect="left(100)"
-                data-duration="3300"
-                data-delay="1900"
-                data-ease="easeOutExpo"
-                ><span class="color-green">FEATURES</span></h3>
-
-            <h3 class="ms-layer normal-title color-light"  style="left:10px; top:312px"
-                data-effect="bottom(20)"
-                data-duration="2300"
-                data-delay="2000"
-                data-ease="easeOutExpo"
-                >UNIFY BOOTSTRAP TEMPLATE</h3>
-
-            <p class="ms-layer ms-promo-sub ms-promo-sub-in color-light"  style="left:10px; top:360px"
-               data-effect="right(40)"
-               data-duration="2300"
-               data-delay="2300"
-               data-ease="easeOutExpo"
-                >YOUTUBE, VIMEO AND CUSTOM <br> IFRAME SUPPORTED</p>
-        </div>
+                <a class="btn-u btn-brd btn-brd-width-2 btn-brd-hover btn-u-light rounded-4x ms-layer ms-promo-sub ms-promo-sub-in color-light"  style="left:10px; top:360px"
+                   data-effect="right(40)"
+                   data-duration="2300"
+                   data-delay="2300"
+                   data-ease="easeOutExpo"
+                   href="#"
+                    >View all</a>
+            </div>
+        <?php } ?>
     </div>
     <!-- end of masterslider -->
 </div>
